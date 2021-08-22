774ea73cb510a7aedab76b0407758ace28e38b13
popehiflo <poolpetterhijuel@hotmail.com>
2021-08-22 00:34:32 -0500

    Primer model y controlador (servicio REST)

--- usuario/src/main/java/pe/popehiflo/gestion/usuario/controllers/UsuarioController.java
+++ usuario/src/main/java/pe/popehiflo/gestion/usuario/controllers/UsuarioController.java
@@ -1,13 +1,53 @@
 package pe.popehiflo.gestion.usuario.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.popehiflo.gestion.usuario.models.Usuario;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public String prueba() {
-        return "prueba controller";
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Pool");
+        usuario.setApellido("Hijuela");
+        usuario.setEmail("email@email.com");
+        usuario.setTelefono("974265159");
+        return usuario;
+    }
+
+    @RequestMapping(value = "editar")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pool");
+        usuario.setApellido("Hijuela");
+        usuario.setEmail("email@email.com");
+        usuario.setTelefono("974265159");
+        return usuario;
+    }
+
+    @RequestMapping(value = "eliminar")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pool");
+        usuario.setApellido("Hijuela");
+        usuario.setEmail("email@email.com");
+        usuario.setTelefono("974265159");
+        return usuario;
+    }
+
+    @RequestMapping(value = "buscar")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pool");
+        usuario.setApellido("Hijuela");
+        usuario.setEmail("email@email.com");
+        usuario.setTelefono("974265159");
+        return usuario;
     }
 }
